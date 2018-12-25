dbdb2087cb20eae8378d9034a845547c905eeebb
e271728182 <gab.patterson@gmail.com>
2018-12-24 20:43:29 -0400

    Add files via upload

---
+++ maddpg.py
@@ -0,0 +1,82 @@
+from ddpg_agent3 import *
+import numpy as np
+
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 512      # minibatch size
+GAMMA = 0.99            # discount factor
+UPDATE_EVERY = 5       # how often to update the network
+NUM_UPDATES=10
+class MADDPG:
+    def __init__(self, agentList,sharedMemory):
+        
+        self.memory =sharedMemory
+                
+        self.ddpg_agents = agentList
+        
+        self.t_step = 0
+     
+    def reset(self):
+        for agent in self.ddpg_agents:
+            agent.reset()
+    
+    def act(self, all_states):
+        """get actions from all agents in the MADDPG object"""
+        actions = [agent.act(np.expand_dims(states, axis=0)) for agent, states in zip(self.ddpg_agents, all_states)]
+        return actions
+    
+    def step(self, states, actions, rewards, next_states, dones):
+        # Save experience in replay memory
+        for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+            self.memory.add(state, action, reward, next_state, done)
+        
+        # Learn every UPDATE_EVERY time steps.
+        
+        
+    def learNiceThings(self):
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) >BATCH_SIZE:
+                for agent in self.ddpg_agents:
+                    for _ in range(NUM_UPDATES):
+                        experiences = self.memory.sample()
+
+                        agent.learn(experiences, GAMMA)
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+        """
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+    
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+    
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
